b1658934017e9415628ba2e4d9d39ac8641b3ba3
wes-santos <weslleyalmerindo2@gmail.com>
2022-07-15 20:04:34 -0300

    feat: create initial folder structure and install initial technologies
    Install Airbnb ESLint, React Router Dom and Styled Components.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <h1 style={{ textAlign: 'center' }}>Desafio XP Front-end</h1>
+    </main>
+  );
+}
+
+export default App;
