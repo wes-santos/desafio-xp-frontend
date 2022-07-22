fd6a23c6ebfb86212c41004570c778d317401f50
wes-santos <weslleyalmerindo2@gmail.com>
2022-07-22 19:41:08 -0300

    test: make modal component tests

---
+++ src/tests/components/modal.test.jsx
@@ -0,0 +1,71 @@
+/* eslint-disable no-undef */
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouterAndRedux from '../../utils/renderWithRouterAndRedux';
+import App from '../../App';
+
+describe('Verifica se', () => {
+  it('Os elementos esperados são exibidos na tela', () => {
+    renderWithRouterAndRedux(<App />, '/negociar');
+
+    const sellButtonEl = screen.getByRole('button', { name: 'Vender' });
+
+    userEvent.click(sellButtonEl);
+
+    const confirmButtonEl = screen.getByRole('button', { name: 'Confirmar' });
+
+    userEvent.click(confirmButtonEl);
+
+    const messageEl = screen.getByText('Você não tem ações suficientes para realizar a transação :(');
+    const depositButtonEl = screen.getByRole('button', { name: 'Fazer depósito' });
+    const backButtonEl = screen.getAllByRole('button', { name: 'Voltar' });
+
+    expect(messageEl).toBeInTheDocument();
+    expect(depositButtonEl).toBeInTheDocument();
+    expect(backButtonEl[0]).toBeInTheDocument();
+    expect(backButtonEl[0].innerHTML).toBe('Voltar');
+  });
+
+  it('Botão de fazer depósito redireciona para a página correta', () => {
+    renderWithRouterAndRedux(<App />, '/negociar');
+
+    const sellButtonEl = screen.getByRole('button', { name: 'Vender' });
+
+    userEvent.click(sellButtonEl);
+
+    const confirmButtonEl = screen.getByRole('button', { name: 'Confirmar' });
+
+    userEvent.click(confirmButtonEl);
+
+    const depositButtonEl = screen.getByRole('button', { name: 'Fazer depósito' });
+
+    userEvent.click(depositButtonEl);
+
+    expect(screen.getByRole('button', { name: 'Depósito' })).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: 'Retirada' })).toBeInTheDocument();
+    expect(screen.getByPlaceholderText('Informe o Valor')).toBeInTheDocument();
+  });
+
+  it('Botão de voltar elimina o modal da tela', () => {
+    renderWithRouterAndRedux(<App />, '/negociar');
+
+    const sellButtonEl = screen.getByRole('button', { name: 'Vender' });
+
+    userEvent.click(sellButtonEl);
+
+    const confirmButtonEl = screen.getByRole('button', { name: 'Confirmar' });
+
+    userEvent.click(confirmButtonEl);
+
+    const messageEl = screen.getByText('Você não tem ações suficientes para realizar a transação :(');
+
+    expect(messageEl).toBeInTheDocument();
+
+    const backButtonEl = screen.getAllByRole('button', { name: 'Voltar' });
+
+    userEvent.click(backButtonEl[0]);
+
+    expect(messageEl).not.toBeInTheDocument();
+  });
+});
